b81efbc5075913f1d5f7b3a3122f7444c202218b
Jorge García Molina <43571181+jorgegarcia1996@users.noreply.github.com>
2020-06-12 14:41:21 +0200

    Boton 'Logout' arreglado

---
+++ App-Web/build/precache-manifest.67338dc23e1ff90cf55f4a646de170e9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6990667a457796a33b4eb09fcd9360cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fe646ec754b40f95991",
+    "url": "/static/css/main.b9c71124.chunk.css"
+  },
+  {
+    "revision": "43b66311b0a545af2f47",
+    "url": "/static/js/2.8665fa76.chunk.js"
+  },
+  {
+    "revision": "a855a7222d00a33eb6846ce89bcfdce5",
+    "url": "/static/js/2.8665fa76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe646ec754b40f95991",
+    "url": "/static/js/main.e53a6023.chunk.js"
+  },
+  {
+    "revision": "faf572691fb2ae01f384",
+    "url": "/static/js/runtime-main.b1dfd432.js"
+  },
+  {
+    "revision": "3054ad2d24fccb757882a776bd4bca85",
+    "url": "/static/media/Icono_persona_arassistant.3054ad2d.svg"
+  },
+  {
+    "revision": "c6ebd6408ae619c5bc047dd1507a7918",
+    "url": "/static/media/bankia.c6ebd640.png"
+  },
+  {
+    "revision": "2162b0929455de31d05e255b281aaf5f",
+    "url": "/static/media/fpempresa.2162b092.png"
+  },
+  {
+    "revision": "00a412a5856423aa3b0c2d98672339ef",
+    "url": "/static/media/iescampanillas.00a412a5.png"
+  },
+  {
+    "revision": "510de4489b400d06afd800d33dd2020d",
+    "url": "/static/media/logo.510de448.svg"
+  }
+]);
